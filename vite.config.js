18228fa48ba8a9e8dfc02376bd9d9a4bbcde2896
techedge-llc <techedgesolutionsllcx@gmail.com>
2025-08-17 17:38:03 -0600

    Fix | Removed old references

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,20 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
+  assetsInclude: ["**/*.jpeg", "**/*.jpg", "**/*.png", "**/*.svg"],
+  build: {
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split(".");
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/[name].[ext]`;
+          }
+          return `assets/[name]-[hash].[ext]`;
+        },
+      },
+    },
+  },
 });
